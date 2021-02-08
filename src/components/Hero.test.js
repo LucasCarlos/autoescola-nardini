c44b10d7e87ff7fbc020b5ff350f8501ed15f093
Lucas Carlos <LucasCarlos@users.noreply.github.com>
2021-02-08 16:53:07 -0300

    Add Hero component

---
+++ src/components/Hero.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Hero from "./Hero";
+
+test("renders Hero with children", () => {
+  const { getByText } = render(
+    <Hero>
+      <p>Lucas Carlos</p>
+    </Hero>
+  );
+
+  expect(getByText("Lucas Carlos")).toBeInTheDocument();
+});
+
+test("renders image background", () => {
+  const image =
+    "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+  const { getByTestId } = render(<Hero image={image} />);
+
+  expect(getByTestId("hero")).toHaveStyleRule({
+    backgroundImage: image,
+  });
+});
