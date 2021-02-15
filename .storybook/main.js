3b6c92196a5b6d6143a50c9c53e7e78dea2b8c06
Lucas Carlos <LucasCarlos@users.noreply.github.com>
2021-02-15 14:12:10 -0300

    Resolve warn in file

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,10 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-docs",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-viewport",
-    "@storybook/addon-docs",
     "@storybook/addon-knobs",
   ],
 };
